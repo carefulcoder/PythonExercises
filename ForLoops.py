1707c7a2d303152ccd76e9841d02a1b3fffbb3bc
carefulcoder <59884103+carefulcoder@users.noreply.github.com>
2020-05-01 16:41:09 -0400

    Add files via upload

--- ForLoops.py
+++ ForLoops.py
@@ -7,4 +7,13 @@ for item in "Python":
 for item in ["John", "Sarah",  "Peter"]:
     print(item)
 
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print('x' * x_count)
+
 # lists items
+# Nested loops:
